9288f46e82d8f0bb8ed2ba52616f534e30e7c559
Hubert depesz Lubaczewski <depesz@depesz.com>
2012-01-11 18:38:46 +0100

    Release version 0.3.1

--- lib/OmniPITR/Program/Monitor.pm
+++ lib/OmniPITR/Program/Monitor.pm
@@ -2,6 +2,6 @@ package OmniPITR::Program::Monitor;
 use strict;
 use warnings;
 
-our $VERSION = '0.3.0';
+our $VERSION = '0.3.1';
 1;
 
--- lib/OmniPITR/Program/Restore.pm
+++ lib/OmniPITR/Program/Restore.pm
@@ -2,7 +2,7 @@ package OmniPITR::Program::Restore;
 use strict;
 use warnings;
 
-our $VERSION = '0.3.0';
+our $VERSION = '0.3.1';
 use base qw( OmniPITR::Program );
 
 use Carp;
