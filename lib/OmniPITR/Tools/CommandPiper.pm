36848eb70515d2dc7f007f4b578fe2da6db44997
Hubert depesz Lubaczewski <depesz@depesz.com>
2012-03-02 21:34:04 +0100

    Looks like working.

--- lib/OmniPITR/Tools/CommandPiper.pm
+++ lib/OmniPITR/Tools/CommandPiper.pm
@@ -1,11 +1,11 @@
-package OmniPITR::Program::Tools::CommandPiper;
+package OmniPITR::Tools::CommandPiper;
 use strict;
 use warnings;
 use English qw( -no_match_vars );
 
 =head1 NAME
 
-OmniPITR::Program::Tools::CommandPiper - Class for building complex pipe-based shell commands
+OmniPITR::Tools::CommandPiper - Class for building complex pipe-based shell commands
 
 =head1 VERSION
 
@@ -19,7 +19,7 @@ our $VERSION = '0.4.0';
 
 General usage is:
 
-    my $run = OmniPITR::Program::Tools::CommandPiper->new( 'ls', '-l' );
+    my $run = OmniPITR::Tools::CommandPiper->new( 'ls', '-l' );
     $run->add_stdout_file( '/tmp/z.txt' );
     $run->add_stdout_file( '/tmp/y.txt' );
     my $checksummer = $run->add_stdout_program( 'md5sum', '-' );
@@ -53,7 +53,7 @@ sub new {
     my $class = shift;
     my $self = bless {}, $class;
     $self->{ 'tee' }             = 'tee';
-    $self->{ 'cmd' }             = [ @ARGV ];
+    $self->{ 'cmd' }             = [ @_ ];
     $self->{ 'stdout_files' }    = [];
     $self->{ 'stdout_programs' } = [];
     $self->{ 'stderr_files' }    = [];
@@ -97,10 +97,9 @@ Add another program that should receive stdout from current program, as its (the
 =cut
 
 sub add_stdout_program {
-    my $self        = shift;
-    my $sub_program = OmniPITR::Program::Tools::CommandPiper->new( @ARGV );
-    push @{ $self->{ 'stdout_programs' } }, $sub_program;
-    return $sub_program;
+    my $self = shift;
+    push @{ $self->{ 'stdout_programs' } }, $self->new_subprogram( @_ );
+    return $self->{ 'stdout_programs' }->[ -1 ];
 }
 
 =head1 add_stderr_file()
@@ -123,9 +122,21 @@ Add another program that should receive stderr from current program, as its (the
 =cut
 
 sub add_stderr_program {
+    my $self = shift;
+    push @{ $self->{ 'stderr_programs' } }, $self->new_subprogram( @_ );
+    return $self->{ 'stderr_programs' }->[ -1 ];
+}
+
+=head1 new_subprogram()
+
+Helper function to create sub-programs, inheriting settings
+
+=cut
+
+sub new_subprogram {
     my $self        = shift;
-    my $sub_program = OmniPITR::Program::Tools::CommandPiper->new( @ARGV );
-    push @{ $self->{ 'stderr_programs' } }, $sub_program;
+    my $sub_program = OmniPITR::Tools::CommandPiper->new( @_ );
+    $sub_program->set_tee_path( $self->{ 'tee' } );
     return $sub_program;
 }
 
@@ -137,6 +148,95 @@ Returns stringified command that should be ran via "system" that does all the de
 
 sub command {
     my $self = shift;
+
+    my $program = join( ' ', map { quotemeta $_ } @{ $self->{ 'cmd' } } );
+
+    my $stderr_redirection = $self->stderr();
+    my $stdout_redirection = $self->stdout();
+
+    $program .= ' ' . $stderr_redirection if $stderr_redirection;
+    $program .= ' ' . $stdout_redirection if $stdout_redirection;
+
+    return $program;
+}
+
+=head1 stdout()
+
+Internal function returning whole stdout redirection for current program, or undef if there are no stdout consummers.
+
+=cut
+
+sub stdout {
+    my $self         = shift;
+    my @stdout_parts = ();
+    push @stdout_parts, map { [ 'PATH', $_ ] } @{ $self->{ 'stdout_files' } };
+    push @stdout_parts, map { [ 'CMD', $_->command() ] } @{ $self->{ 'stdout_programs' } };
+    return if 0 == scalar @stdout_parts;
+
+    my $ready = $self->tee_maker( @stdout_parts );
+    return sprintf( '> %s', $ready->[ 1 ] ) if 'PATH' eq $ready->[ 0 ];
+    return sprintf( '| %s', $ready->[ 1 ] );
+}
+
+=head1 stderr()
+
+Internal function returning whole stderr redirection for current program, or undef if there are no stderr consummers.
+
+=cut
+
+sub stderr {
+    my $self         = shift;
+    my @stderr_parts = ();
+    push @stderr_parts, map { [ 'PATH', $_ ] } @{ $self->{ 'stderr_files' } };
+    push @stderr_parts, map { [ 'CMD',  $_->command() ] } @{ $self->{ 'stderr_programs' } };
+    return if 0 == scalar @stderr_parts;
+
+    my $ready = $self->tee_maker( @stderr_parts );
+    return sprintf( '2> %s', $ready->[ 1 ] ) if 'PATH' eq $ready->[ 0 ];
+    return sprintf( '2> >( %s )', $ready->[ 1 ] );
+}
+
+=head1 tee_maker()
+
+Receives array of arrayrefs. Each element is array with two values:
+
+=over
+
+=item 1. type of element "PATH" - path to file, "CMD" - command to run
+
+=item 2. path to file, or command line of program to run.
+
+=back
+
+Returns single item of [ "PATH", "/path/to/file" ] or [ "CMD", "command to run" ] that will deliver data to all receivers.
+
+=cut
+
+sub tee_maker {
+    my $self  = shift;
+    my @parts = @_;
+    if ( 1 == scalar @parts ) {
+        $parts[ 0 ]->[ 1 ] = quotemeta( $parts[ 0 ]->[ 1 ] ) if 'PATH' eq $parts[ 0 ]->[ 0 ];
+        return $parts[ 0 ];
+    }
+
+    my $last     = pop @parts;
+    my @tee_args = ();
+    for my $p ( @parts ) {
+        if ( 'PATH' eq $p->[ 0 ] ) {
+            push @tee_args, quotemeta $p->[ 1 ];
+            next;
+        }
+        push @tee_args, sprintf( '>( %s )', $p->[ 1 ] );
+    }
+    my $tee_invocation = sprintf '%s %s', quotemeta( $self->{ 'tee' } ), join( ' ', @tee_args );
+    if ( 'PATH' eq $last->[ 0 ] ) {
+        $tee_invocation .= ' > ' . quotemeta( $last->[ 1 ] );
+    }
+    else {
+        $tee_invocation .= ' | ' . $last->[ 1 ];
+    }
+    return [ 'CMD', $tee_invocation ];
 }
 
 1;
